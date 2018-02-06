b5ba826f5ad577f927ecee9b99cbe642b6a5ab5e
Gerardo Bonilla <gerbonilla@gmail.com>
2018-02-06 22:46:49 +0100

    added images to first screen

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,22 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    @magazines = [
+      "Archive_I_2.png",
+      "Archive_I_3.png",
+      "BQ_I_3.jpg",
+      "BQ_I_4.jpg",
+      "BQ_I_5.jpg",
+      "BQ_I_6.jpg",
+      "BQ_I_7.jpg",
+      "Flaneur_I_3.jpg",
+      "Flaneur_I_4.jpg",
+      "Kaltblut_I_2.jpg",
+      "Kaltblut_I_3.jpg",
+      "TTA_I_1.jpg",
+      "TTA_I_11.jpg",
+      "TTA_I_12.jpg",
+      "TTA_I_2.jpg",
+      "TTA_I_3.jpg"]
   end
 end
